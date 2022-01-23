a13fd20441e24369bca5e8526edf7ec4a9044989
Wotaker <wotaker@DESKTOP-37QMHRM.localdomain>
2022-01-23 17:09:33 +0100

    if statements

---
+++ AdaFiles/factorial10loop.adb
@@ -0,0 +1,11 @@
+procedure Factorial10loop is
+    b_res: Boolean := TRUE;
+    acc: Integer := 1;
+    i: Integer := 1;
+begin
+
+for i in range 1..7
+
+-- Put_Line(Integer'Image(acc));
+    
+end Factorial10loop;
